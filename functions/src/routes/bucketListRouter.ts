d18356f73ac7956a56b68cc5a63bc1564a9b6024
Chris Calleja <callejac1@gmail.com>
2022-08-29 14:57:46 -0400

    backend stuff

--- functions/src/routes/bucketListRouter.ts
+++ functions/src/routes/bucketListRouter.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { ObjectId } from "mongodb";
 import { getClient } from "../db";
 import BucketListItem from "../models/BucketListItem";
 
@@ -18,6 +19,39 @@ bucketListRouter.get("/", async (req, res) => {
       .find()
       .toArray();
     res.json(results);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+bucketListRouter.post("/", async (req, res) => {
+  try {
+    const client = await getClient();
+    const newItem: BucketListItem = req.body;
+    await client
+      .db()
+      .collection<BucketListItem>("bucketList")
+      .insertOne(newItem);
+    res.status(200);
+    res.json(newItem);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+bucketListRouter.delete("/user/:id", async (req, res) => {
+  try {
+    const id: string = req.params.id;
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<BucketListItem>("bucketList")
+      .deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send("ID Not Found");
+    }
   } catch (err) {
     errorResponse(err, res);
   }
