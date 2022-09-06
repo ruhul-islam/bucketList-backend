519f7889d751b277cd6fd7f4c6a06437b3956ac5
ruhul-islam <islamruhul48092@gmail.com>
2022-09-06 09:13:22 -0400

    weekend stuff

--- functions/src/models/BucketListItem.ts
+++ functions/src/models/BucketListItem.ts
@@ -1,6 +1,8 @@
 export default interface BucketListItem {
+  completed: boolean;
   idea: string;
   date: string;
   category: string;
   consequence: string;
+  isPrivate: boolean;
 }
--- functions/src/routes/userRouter.ts
+++ functions/src/routes/userRouter.ts
@@ -11,6 +11,22 @@ const errorResponse = (error: any, res: any) => {
   res.status(500).json({ message: "Internal Server Error" });
 };
 
+userRouter.get("/allUsers", async (req, res) => {
+  try {
+    const client = await getClient();
+    const results = await client
+      .db()
+      .collection<User[]>("users")
+      .find({})
+      .toArray();
+    console.log(results);
+    res.status(200);
+    res.json(results);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
 userRouter.get("/:uid", async (req, res) => {
   try {
     const uid: string = req.params.uid;
@@ -27,6 +43,30 @@ userRouter.get("/:uid", async (req, res) => {
   }
 });
 
+///////
+userRouter.get("/friends/:uid", async (req, res) => {
+  try {
+    const uid: string = req.params.uid;
+    const client = await getClient();
+    const result = await client.db().collection<User>("users").findOne({ uid });
+    let results: User[] | [] = [];
+    for (let index = 0; index < result?.following?.length!; index++) {
+      const element: string | undefined = result?.following![index];
+      const singleFriend: User | null = await client
+        .db()
+        .collection<User>("users")
+        .findOne({ uid: element });
+      results.push(singleFriend);
+    }
+    console.log(results);
+    res.status(200);
+    res.json(results);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+///////
+
 userRouter.post("/", async (req, res) => {
   try {
     const client = await getClient();
@@ -40,9 +80,11 @@ userRouter.post("/", async (req, res) => {
 });
 
 userRouter.put("/:uid/bucket-list/add", async (req, res) => {
+  console.log("here");
   try {
     const uid: string = req.params.uid;
     const client = await getClient();
+    req.body["completed"] = false;
     const newBucketListItem: BucketListItem = req.body;
     const results = await client
       .db()
@@ -62,11 +104,12 @@ userRouter.put("/:uid/bucket-list/add", async (req, res) => {
 });
 
 userRouter.put("/:uid/bucket-list/remove", async (req, res) => {
+  //console.log("idea", req.body);
   try {
     const uid: string = req.params.uid;
     // const index: number = parseInt(req.params.index);
     const idea: string = req.body.idea;
-    console.log(idea);
+    console.log("my idea", idea);
     const client = await getClient();
     const results = await client
       .db()
@@ -78,7 +121,190 @@ userRouter.put("/:uid/bucket-list/remove", async (req, res) => {
         }
       );
     if (results.modifiedCount) {
-      res.sendStatus(200);
+      res.status(200).json({
+        success: true,
+        message: "deleted",
+      });
+    } else {
+      res.status(404);
+      res.send("User Not Found");
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+userRouter.put("/:uid/bucket-list/complete", async (req, res) => {
+  //console.log("idea", req.body);
+  try {
+    const uid: string = req.params.uid;
+    // const index: number = parseInt(req.params.index);
+    const idea: string = req.body.idea;
+    console.log("my idea", idea);
+    const client = await getClient();
+
+    const results1 = await client
+      .db()
+      .collection<User>("users")
+      .findOne({ uid });
+    //console.log(results);
+
+    const filtered = results1?.bucketList?.filter((item) => {
+      if (item.idea === idea) {
+        item.completed = true;
+      }
+      return item;
+    });
+
+    const results = await client
+      .db()
+      .collection<User>("users")
+      .updateOne(
+        { uid },
+        {
+          $set: { bucketList: filtered },
+        }
+      );
+    if (results.modifiedCount) {
+      res.status(200).json({
+        success: true,
+        message: "deleted",
+      });
+    } else {
+      res.status(404);
+      res.send("User Not Found");
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+userRouter.put("/:uid/bucket-list/incomplete", async (req, res) => {
+  //console.log("idea", req.body);
+  try {
+    const uid: string = req.params.uid;
+    // const index: number = parseInt(req.params.index);
+    const idea: string = req.body.idea;
+    console.log("my idea", idea);
+    const client = await getClient();
+
+    const results1 = await client
+      .db()
+      .collection<User>("users")
+      .findOne({ uid });
+    //console.log(results);
+
+    const filtered = results1?.bucketList?.filter((item) => {
+      if (item.idea === idea) {
+        item.completed = false;
+      }
+      return item;
+    });
+
+    const results = await client
+      .db()
+      .collection<User>("users")
+      .updateOne(
+        { uid },
+        {
+          $set: { bucketList: filtered },
+        }
+      );
+    if (results.modifiedCount) {
+      res.status(200).json({
+        success: true,
+        message: "not completed",
+      });
+    } else {
+      res.status(404);
+      res.send("User Not Found");
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+userRouter.put("/:uid/bucket-list/private", async (req, res) => {
+  //console.log("idea", req.body);
+  try {
+    const uid: string = req.params.uid;
+    // const index: number = parseInt(req.params.index);
+    const idea: string = req.body.idea;
+    console.log("my idea", idea);
+    const client = await getClient();
+
+    const results1 = await client
+      .db()
+      .collection<User>("users")
+      .findOne({ uid });
+    //console.log(results);
+
+    const filtered = results1?.bucketList?.filter((item) => {
+      if (item.idea === idea) {
+        item.isPrivate = true;
+      }
+      return item;
+    });
+
+    const results = await client
+      .db()
+      .collection<User>("users")
+      .updateOne(
+        { uid },
+        {
+          $set: { bucketList: filtered },
+        }
+      );
+    if (results.modifiedCount) {
+      res.status(200).json({
+        success: true,
+        message: "made private",
+      });
+    } else {
+      res.status(404);
+      res.send("User Not Found");
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+userRouter.put("/:uid/bucket-list/public", async (req, res) => {
+  //console.log("idea", req.body);
+  try {
+    const uid: string = req.params.uid;
+    // const index: number = parseInt(req.params.index);
+    const idea: string = req.body.idea;
+    console.log("my idea", idea);
+    const client = await getClient();
+
+    const results1 = await client
+      .db()
+      .collection<User>("users")
+      .findOne({ uid });
+    //console.log(results);
+
+    const filtered = results1?.bucketList?.filter((item) => {
+      if (item.idea === idea) {
+        item.isPrivate = false;
+      }
+      return item;
+    });
+
+    const results = await client
+      .db()
+      .collection<User>("users")
+      .updateOne(
+        { uid },
+        {
+          $set: { bucketList: filtered },
+        }
+      );
+    if (results.modifiedCount) {
+      res.status(200).json({
+        success: true,
+        message: "made public",
+      });
     } else {
       res.status(404);
       res.send("User Not Found");
